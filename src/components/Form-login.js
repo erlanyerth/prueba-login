6646f31adbda23b3d41f37e1bc5581e2d1461ad3
Erlanyerth Sorett <erlanyerth@gmail.com>
2021-04-30 21:30:54 -0430

    added localStorage for token

--- src/components/Form-login.js
+++ src/components/Form-login.js
@@ -51,7 +51,7 @@ class Form extends React.Component {
            })
         .then((response) =>{
             console.log(response);
-            localStorage.setItem("token", response.data.sessionTokenBck); //guarda el token en el navegador
+            localStorage.setItem("token", response.data.sessionTokenBck); //guarda el token en el navegador para usarlo posteriormente
             //this.props.history.push("/home");
             //if (response.data.status === "ok"){
             //    console.log(response);
